1ca249f9acf1c2139e8be63b0ee0bec83de95a04
hcaxt <hcaxtonmartins@gmail.com>
2023-04-21 13:19:28 +0100

    removed aws access credentials

--- kinesis_settings.py
+++ kinesis_settings.py
@@ -3,8 +3,8 @@
 
 class KinesisSettings(object):
     def __init__(self):
-        self.aws_access_key = "AKIAT445OCI5UG2AA45K"
-        self.aws_secret_key = "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"
+        self.aws_access_key = "xxxxxxxxx"
+        self.aws_secret_key = "xxxxxxxxxx"
         self.aws_region = "eu-west-2"
         self.stream_name = "PUT-S3-acHd2"
         
